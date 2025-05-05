e41c5c1873d7e4d9cb85b988163fc58151736a75
sovon singha <sovonsingha9@gmail.com>
2025-05-05 12:49:36 +0530

    5th may

--- src/main/java/com/plantapps/MessageProducer/controller/MessageController.java
+++ src/main/java/com/plantapps/MessageProducer/controller/MessageController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plantapps.MessageProducer.kafka.KafkaMessageProducer;
@@ -15,8 +16,8 @@ public class MessageController
 	@Autowired
 	private KafkaMessageProducer producer;
 	
-	@PostMapping("/message/{tid}")
-	public void produceMessageData(@PathVariable("tid") String tid, @RequestBody Message message)
+	@PostMapping("/message")
+	public void produceMessageData(@RequestHeader("tid") String tid, @RequestBody Message message)
 	{
 		//Message messsage  = new Message(1L, data);
 		producer.produceMesageTenents(tid , message);
